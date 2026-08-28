e87f8f1e8f236bbcf4f16e3f1c7b5c597c974da0
Loujina Nader <loujinanader11@gmail.com>
2026-08-29 01:25:23 +0300

    Add UpdateStudentRequestValidator with FluentValidation
    Added UpdateStudentRequestValidator class in School.Api.Validators using FluentValidation. Enforces validation rules for StudentName, Age, and CID in update requests.

---
+++ School.Api/Validators/UpdateStudentRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using School.Api.DTO.Request;
+namespace School.Api.Validators
+{
+    public class UpdateStudentRequestValidator : AbstractValidator<UpdateStudentRequestValidator>
+    {
+        public UpdateStudentRequestValidator()
+        {
+            RuleFor(x => x.StudentName)
+            .NotEmpty().WithMessage("StudentName is required.")
+            .MaximumLength(50).WithMessage("StudentName cannot exceed 200 characters.");
+
+            RuleFor(x => x.Age)
+                .InclusiveBetween(5, 18);
+
+            RuleFor(x => x.CID)
+                   .GreaterThan(0);
+        }
+    }
+}
