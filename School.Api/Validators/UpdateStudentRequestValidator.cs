26f22efcc508a941ce1bd4c0202ed64f6eafe003
Loujina Nader <loujinanader11@gmail.com>
2026-08-29 01:35:13 +0300

    Refactor and simplify student validator class
    Renamed UpdateStudentRequestValidator to UpdateStudentValidator and updated it to validate CreateStudentRequest. Simplified StudentName validation by removing error messages and correcting max length to 50. Applied minor formatting improvements for consistency.

--- School.Api/Validators/UpdateStudentRequestValidator.cs
+++ School.Api/Validators/UpdateStudentRequestValidator.cs
@@ -1,20 +1,21 @@
 ﻿using FluentValidation;
 using School.Api.DTO.Request;
+
 namespace School.Api.Validators
 {
-    public class UpdateStudentRequestValidator : AbstractValidator<UpdateStudentRequestValidator>
+    public class UpdateStudentValidator : AbstractValidator<CreateStudentRequest>
     {
-        public UpdateStudentRequestValidator()
+        public UpdateStudentValidator()
         {
             RuleFor(x => x.StudentName)
-            .NotEmpty().WithMessage("StudentName is required.")
-            .MaximumLength(50).WithMessage("StudentName cannot exceed 200 characters.");
+                .NotEmpty()
+                .MaximumLength(50);
 
             RuleFor(x => x.Age)
                 .InclusiveBetween(5, 18);
 
             RuleFor(x => x.CID)
-                   .GreaterThan(0);
+                .GreaterThan(0);
         }
     }
 }
