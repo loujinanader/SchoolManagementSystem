117e2bd82e96b689d69d48011e666bcb4c6d6d28
Loujina Nader <loujinanader11@gmail.com>
2026-08-30 10:39:15 +0300

    Add Users table migration and update project secrets config
    Added UpdateUserModel EF migration to create Users table with Id, UserName, PasswordHash, and Role columns. Updated SchoolDbContextModelSnapshot and migration designer file. Modified School.Api.csproj to include UserSecretsId for secret management.

--- School.Api/Migrations/20260830073336_UpdateUserModel.Designer.cs
+++ School.Api/Migrations/20260830073336_UpdateUserModel.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using School.Api.Data;
 
@@ -10,9 +11,11 @@ using School.Api.Data;
 namespace School.Api.Migrations
 {
     [DbContext(typeof(SchoolDbContext))]
-    partial class SchoolDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260830073336_UpdateUserModel")]
+    partial class UpdateUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
